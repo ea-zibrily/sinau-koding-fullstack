bb99e337d790435b38b76b2f6b340dcacea74b30
Shinon <zyuuannn@gmail.com>
2025-09-06 21:48:38 +0700

    feat: javascript fundamental

---
+++ front-end/script.js
@@ -0,0 +1,20 @@
+// Primitive
+let userName = "ea";
+let uniqueID = 223014;
+let isLoginToday = false;
+
+
+console.log("username: %s", userName);
+console.log("unique: %d", uniqueID);
+console.log("isLoginToday:", isLoginToday);
+
+// Non-Primitive
+let inventory = ["Headphone", "Mouspad", "Gamepad"];
+let playerData = {
+    id: 77563432,
+    name: "최지우",
+    role: "Captain"
+}
+
+console.log("using item:", inventory[0]);
+console.log(playerData);
